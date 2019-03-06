4a017fa9717901c15163a124c3efb48f24770be9
Dzmitriy <Dzmitriy_Kakhanouski@epam.com>
2019-03-06 11:31:55 +0300

    first commit

---
+++ firstTaskDotNetWF/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestLib;
+
+namespace firstTaskDotNetWF
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      UserManager userManager = new UserManager(textBox1.Text);
+
+
+      MessageBox.Show(userManager.GetGreeting(), "Welcome",MessageBoxButtons.OK);
+    }
+  }
+}
